4a50a1c8c53e10751458c9231a2af351b0dec55f
Guddu_kumar <gudduaur2016@gmail.com>
2024-09-27 19:57:32 +0530

    Update PIMLINK.java

--- PIMLINK.java
+++ PIMLINK.java
@@ -23,24 +23,7 @@ public class PIMLINK extends Base_class
 		
 		p.emp_del().click();
 		p.emp_delcnf().click();
-		
-//	driver.findElement(By.xpath("//span[.='PIM']")).click();  //click  on PIM
-//
-//	driver.findElement(By.xpath("//a[.='Add Employee']")).click(); //click on add employee
-//	
-//		
-//	driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(Generic_DDT.read_data("sheet2", 3, 1));  // Enter the firstname
-//	
-//	driver.findElement(By.xpath("//input[@placeholder='Middle Name']")).sendKeys(Generic_DDT.read_data("sheet2", 4, 1));;   // Enter the MiddleName
-//	
-//	driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(Generic_DDT.read_data("sheet2", 5, 1 ),Keys.ENTER);   //Enter the LastName
-//
-//		driver.findElement(By.xpath("//a[.='Employee List']")).click();
-//		driver.findElement(By.xpath("(//input[@placeholder='Type for hints...'])[1]")).sendKeys("guddu kr. saxena");
-//		driver.findElement(By.xpath("//button[.=' Search ']")).click();
-//		driver.findElement(By.xpath("//i[@class='oxd-icon bi-trash']")).click();
-//		driver.findElement(By.xpath("//button[@type='button' and @class='oxd-button oxd-button--medium oxd-button--label-danger orangehrm-button-margin']")).click();
-//	
+
 
 }
 }
